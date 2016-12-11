851be00fbfe61f9cf2a11d654ebb2a3e47778613
edrichhans <edrichhans@yahoo.com>
2016-12-12 01:24:22 +0800

    changed sql file

--- ticketing.sql
+++ ticketing.sql
@@ -1,58 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2016 at 06:23 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
-CREATE TABLE User(
-	userID 		INT 		NOT NULL,
-	username	VARCHAR(10) NOT NULL,
-	name		VARCHAR(50)	NOT NULL,
-	password 	VARCHAR(10) NOT NULL,
-	email		VARCHAR(20),
-	phone		VARCHAR(20),
-	PRIMARY KEY(userID)
-);
-
-CREATE TABLE Event(
-	eventID 	INT			NOT NULL,
-	eventName	VARCHAR(50)	NOT NULL,
-	actors		VARCHAR(100),
-	synopsis	VARCHAR(200),
-	PRIMARY KEY(eventID)
-);
-
-CREATE TABLE Theater(
-	theaterID	INT			NOT NULL,
-	theatername	VARCHAR(50) NOT NULL,
-	noOfSeats	INT 		NOT NULL,
-	location	VARCHAR(100) NOT NULL,
-	PRIMARY KEY(theaterID)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-CREATE TABLE Created(
-	userID 		INT		NOT NULL,
-	eventID		INT		NOT NULL,
-	dateCreated	DATE 	NOT NULL,
-	PRIMARY KEY(userID,eventID)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE Shows(
-	showID		INT		NOT NULL,
-	eventID		INT 	NOT NULL,
-	theaterID	INT 	NOT NULL,
-	showDate	DATE 	NOT NULL,
-	startTime	TIME 	NOT NULL,
-	endTime		TIME 	NOT NULL,
-	PRIMARY KEY(showID)
-);
+--
+-- Database: `ticketing`
+--
+CREATE DATABASE IF NOT EXISTS `ticketing` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `ticketing`;
 
-CREATE TABLE Tickets(
-	ticketNo	INT		NOT NULL,
-	showID		INT		NOT NULL,
-	isReserved	INT		NOT NULL,
-	PRIMARY KEY(ticketNo)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE Reserved(
-	ticketNo 	INT 	NOT NULL,
-	userID 		INT 	NOT NULL,
-	dateReserved	DATETIME NOT NULL,
-	PRIMARY KEY (ticketNo,userID)
-);
+--
+-- Table structure for table `created`
+--
+
+CREATE TABLE `created` (
+  `userID` int(11) NOT NULL,
+  `eventID` int(11) NOT NULL,
+  `dateCreated` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `eventID` int(11) NOT NULL,
+  `eventName` varchar(50) NOT NULL,
+  `actors` varchar(100) DEFAULT NULL,
+  `synopsis` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reserved`
+--
+
+CREATE TABLE `reserved` (
+  `ticketNo` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `dateReserved` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shows`
+--
+
+CREATE TABLE `shows` (
+  `showID` int(11) NOT NULL,
+  `eventID` int(11) NOT NULL,
+  `theaterID` int(11) NOT NULL,
+  `showDate` date NOT NULL,
+  `startTime` time NOT NULL,
+  `showEndDate` date NOT NULL,
+  `endTime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `theater`
+--
+
+CREATE TABLE `theater` (
+  `theaterID` int(11) NOT NULL,
+  `theatername` varchar(50) NOT NULL,
+  `noOfSeats` int(11) NOT NULL,
+  `location` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `ticketNo` int(11) NOT NULL,
+  `showID` int(11) NOT NULL,
+  `isReserved` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `rights` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `created`
+--
+ALTER TABLE `created`
+  ADD PRIMARY KEY (`userID`,`eventID`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`eventID`);
+
+--
+-- Indexes for table `reserved`
+--
+ALTER TABLE `reserved`
+  ADD PRIMARY KEY (`ticketNo`,`userID`);
+
+--
+-- Indexes for table `shows`
+--
+ALTER TABLE `shows`
+  ADD PRIMARY KEY (`showID`);
+
+--
+-- Indexes for table `theater`
+--
+ALTER TABLE `theater`
+  ADD PRIMARY KEY (`theaterID`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`ticketNo`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `eventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+--
+-- AUTO_INCREMENT for table `shows`
+--
+ALTER TABLE `shows`
+  MODIFY `showID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+--
+-- AUTO_INCREMENT for table `theater`
+--
+ALTER TABLE `theater`
+  MODIFY `theaterID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `ticketNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=366;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
